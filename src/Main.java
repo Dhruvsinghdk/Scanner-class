b1ec966e9540cf769f6a4c11939c123bc39c9ac6
Dhruvsinghdk <re621006@gmail.com>
2024-08-27 14:41:27 +0530

    new scanner

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner n = new Scanner(System.in);
 
+        System.out.println("Enter x :");
+        int x = n.nextInt();
+        System.out.println("Enter y :");
+        int y = n.nextInt();
+        int z = x+y;
+        System.out.println("Sum = "+z);
     }
 }
