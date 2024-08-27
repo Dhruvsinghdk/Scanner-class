00f0faef9e7105b59c9a57b1beef17f8bd460eee
Dhruvsinghdk <re621006@gmail.com>
2024-08-27 16:06:36 +0530

    new sc

---
+++ src/Binary.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Binary {
+    public static void main(String[] args) {
+        Scanner n = new Scanner(System.in);
+        int x = n.nextInt(16);
+        int y = n.nextInt(8);
+        int z = n.nextInt(2);
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+    }
+}
